4a240e35d2280d8f68e66d05d43be8bbb63bf79d
Florian <florian.terisse@gmail.com>
2024-04-03 00:06:49 +0200

    Replacements

--- src/main/java/fr/terisse/api/notifsapi/utils/ReplacementUtils.java
+++ src/main/java/fr/terisse/api/notifsapi/utils/ReplacementUtils.java
@@ -16,10 +16,14 @@ public class ReplacementUtils {
         replacements.put("CA VSAV", "chef d'agrès VSAV");
         replacements.put("CA FPT", "chef d'agrès VSAV");
         replacements.put("CE FPT", "chef d'équipe FPT");
+        replacements.put("EQ1 VSAV", "équipier 1 VSAV");
+        replacements.put("EQ2 VSAV", "équipier 2 VSAV");
         replacements.put("Depart", "Départ");
         replacements.put("TERISSE", "TÉRISSE");
         replacements.put("SAP REG", "Secours à personne - Régulation -");
         replacements.put("FEU D HABITATION INDIVIDUELLE", "Feu d'habitation individuelle");
+        replacements.put("SAP REG", "Secours à personne régulé");
+        replacements.put("CLUGERY VOLODIA", "Clugery Volodia");
     }
 
     public String makeReplacements(String message) {
